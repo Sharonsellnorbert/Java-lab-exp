46e19ed3d9130705b8db2de1b5f944f42f13e89a
Josna2023 <166109567+Josna2023@users.noreply.github.com>
2024-08-22 11:11:12 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,17 @@
+//area(rectangle)
+import java.util.Scanner;
+public class Area
+{
+   public static void main(String args[])
+   {
+     Scanner sc=new Scanner(System.in);
+     int l,b,a;
+     System.out.println("enter the length");
+     num=sc.nextInt();
+     System.out.println("enter the breadth");
+     num=sc.nextInt();
+     a=l*b;
+     System.out.println("area of the rectangle="+a);
+     
+   }
+}
